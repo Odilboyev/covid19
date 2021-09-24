63a78bd6ce7780e96f7d38030b913428bdba73d0
odilboyev <webiusdev@gmail.com>
2021-09-24 21:20:48 +0500

    removed 'recovered' section

--- main.js
+++ main.js
@@ -1,47 +1,44 @@
-const btn = document.getElementById('submit')
+const btn = document.getElementById("submit");
 
 // get covid data from api
 const fetchData = async (value) => {
-    let response = await fetch(`https://covid19.mathdro.id/api/countries/${value}`)
-    let json = await response.json()
-    let data = []
-    data.push(json.confirmed.value)
-    data.push(json.deaths.value)
-    data.push(json.recovered.value)
-    return data
-}
+  let response = await fetch(
+    `https://covid19.mathdro.id/api/countries/${value}`
+  );
+  let json = await response.json();
+  let data = [];
+  data.push(json.confirmed.value);
+  data.push(json.deaths.value);
+  data.push(json.recovered.value);
+  return data;
+};
 
 // get value for find country and display results
 btn.onclick = async () => {
-    let input = document.getElementById('country').value;
-    display.innerHTML = '<div class="lds-circle"><div></div></div>'
-    let response = await fetchData(input)
+  let input = document.getElementById("country").value;
+  display.innerHTML = '<div class="lds-circle"><div></div></div>';
+  let response = await fetchData(input);
+  console.log(response);
+  if (response.length === 0) {
+    display.innerHTML = `<h2 class="text-center">Please, Enter the country correctly</h2>`;
+  } else {
+    display.innerHTML = `
+    <h2 class="text-center">Covid-19 data</h2>
+        <div class="row shadow rounded my-5">
+        <div class="col p-3 border  d-flex justify-content-center align-items-center flex-column">
+            <h4 class="text-warning">Confirmed</h4>
 
-    
-    if (response.length === 0) {
-        display.innerHTML = `<h2 class="text-center">Please, Enter the state correctly</h2>`
-    }
+            <h5 id="confrimed">${response[0]}</h5>
+        </div>
 
-    else {
-        display.innerHTML = `<h2 class="text-center">Covid-19 data</h2>
-            <div class="row shadow rounded my-5">
-            <div class="col p-3 border  d-flex justify-content-center align-items-center flex-column">
-                <h4 class="text-warning">Confirmed</h4>
-    
-                <h5 id="confrimed">${response[0]}</h5>
-            </div>
-            <div class="col p-3 border d-flex justify-content-center align-items-center flex-column">
-                <h4 class="text-success">Recovered</h4>
-    
-                <h5 id="recovered">${response[2]}</h5>
-            </div>
-    
-            <div class="col p-3 border  d-flex justify-content-center align-items-center flex-column">
-                <h4 class="text-danger">Deaths</h4>
-    
-                <h5 id="deaths">${response[1]}</h5>
-            </div>
-            </div>`
-    }
+        <div class="col p-3 border  d-flex justify-content-center align-items-center flex-column">
+            <h4 class="text-danger">Deaths</h4>
 
-}
+            <h5 id="deaths">${response[1]}</h5>
+        </div>
+    </div>
+    
+    <h5 class="text-center" > Search for <i> covid data ${input}</i> from google to check :) </h5>
+    `;
+  }
+};
